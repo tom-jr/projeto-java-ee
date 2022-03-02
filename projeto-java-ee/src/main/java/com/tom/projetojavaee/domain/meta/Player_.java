fc3c60fb1b1a101d4c6de466a39196fc54e05a36
tom-jr <tomjavastdy@gmail.com>
2022-03-02 08:19:50 -0300

    #004 -  Criteria API
    Utilizando Metamodel para o criteria from

---
+++ projeto-java-ee/src/main/java/com/tom/projetojavaee/domain/meta/Player_.java
@@ -0,0 +1,20 @@
+package com.tom.projetojavaee.domain.meta;
+
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+import com.tom.projetojavaee.domain.Player;
+import com.tom.projetojavaee.domain.Team;
+
+@StaticMetamodel(Player.class)
+public class Player_ {
+    
+    // public static volatile SingularAttribute<Player, Long> id;
+    // public static volatile SingularAttribute<Player, String> name;
+    // public static volatile SingularAttribute<Player, String> position;
+    // public static volatile SingularAttribute<Player, Boolean> salary;
+    // public static volatile ListAttribute<Player, Team> teams;
+
+
+}
